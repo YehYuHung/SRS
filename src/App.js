27e00ec1272619acbaed8ccccda71b1b0161259f
morga.yeh <morga.yeh@hy-tech.com.tw>
2024-02-03 17:11:21 +0800

    [SRS] 架構資料夾添加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import Navbar from './component/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
