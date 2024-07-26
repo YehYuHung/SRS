9359bf293b4a985b13cf4c312c6bfac53259e2eb
Morga.Yeh <morga.ye@e-funtech.com>
2024-07-26 12:28:10 +0800

    [SRS] Package 整理調整專案

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Navbar from './component/navbar';
+import Navbar from './components/navbar';
 
 function App() {
   return (
